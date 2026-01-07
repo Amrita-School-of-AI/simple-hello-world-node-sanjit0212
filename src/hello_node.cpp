0feb49d92ba7c118f2289830c47f455f6ff9a740
sanjit0212 <sanjit02122006@gmail.com>
2026-01-07 09:28:53 +0530

    Completed ROS2 Hello World Assignment

---
+++ src/hello_node.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+class HelloNode : public rclcpp::Node
+{
+public:
+  HelloNode()
+  : Node("hello_world_node")
+  {
+    timer_ = this->create_wall_timer(
+      1000ms, std::bind(&HelloNode::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    RCLCPP_INFO(this->get_logger(), "Hello, World!");
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<HelloNode>());
+  rclcpp::shutdown();
+  return 0;
+}
